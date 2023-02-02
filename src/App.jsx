6befe4b84299c913d6e1206474f68739c0e1c018
AVIRAL GUPTA <guptaaviral59@gmail.com>
2023-02-03 07:22:10 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -162,9 +162,7 @@ function App() {
                             <div className=" bg-white sm:p-6">
                               <MeVideoElem />
                               <a href="#">
-                                <h3 className="mt-4 text-center text-lg font-medium text-gray-900">
-                                  User 1
-                                </h3>
+                                
                               </a>
 
 
@@ -179,9 +177,7 @@ function App() {
 
                       ))}
                       <a href="#">
-                        <h3 className="mt-4 text-center text-lg font-medium text-gray-900">
-                          User 1
-                        </h3>
+                       
                       </a>
 
 
@@ -191,23 +187,29 @@ function App() {
                         </div>
 
                         <div className="mt-8 text-center align-middle">
-                          <a href="#_" class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+                          <button onClick={handleJoin} class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
                             <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
                             <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                             <span class="relative text-white">Join Room</span>
-                          </a>
+                          </button>
 
-                          <a href="#_" class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+                          <button onClick={() => huddleClient.enableWebcam()} class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
                             <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
                             <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                             <span class="relative text-white">Enable Cam</span>
-                          </a>
+                          </button>
 
-                          <a href="#_" class="relative m-1 inline-flex items-center justify-center p-4 px-10 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+                          <button onClick={() => huddleClient.disableWebcam()} class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+                            <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
+                            <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                            <span class="relative text-white">Disable Cam</span>
+                          </button>
+
+                          <button onClick={() => handleSkip()} class="relative m-1 inline-flex items-center justify-center p-4 px-10 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
                             <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
                             <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                             <span class="relative text-white">Skip</span>
-                          </a>
+                          </button>
                         </div>
                       </div>
                     </figure>
@@ -216,25 +218,13 @@ function App() {
 
               </div>
 
-              <div className="">
-                <div className="">
-             
-
-                </div>
-              </div>
             </div>
             <div className="">
-              <button onClick={handleJoin}>Join Room</button>
-              <button onClick={() => huddleClient.enableWebcam()}>
-                Enable Webcam
-              </button>
-              <button onClick={() => huddleClient.disableWebcam()}>
-                Disable Webcam
-              </button>
+         
               <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
                 allowAllLobbyPeersToJoinRoom()
               </button>
-              <button onClick={() => handleSkip()}>
+              <button >
                 Skip
               </button>
             </div>
