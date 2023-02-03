af9eef51cb8dd0f67cf97580c110c105536f1dec
AVIRAL GUPTA <guptaaviral59@gmail.com>
2023-02-03 08:06:39 +0800

    h

--- src/App.jsx
+++ src/App.jsx
@@ -194,6 +194,12 @@ function App() {
                             <span class="relative text-white">Join Room</span>
                           </button>
 
+                          <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()} class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+                            <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
+                            <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                            <span class="relative text-white">Add Member</span>
+                          </button>
+
                           <button onClick={() => huddleClient.enableWebcam()} class="relative m-1 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
                             <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-violet-400"></span>
                             <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
@@ -222,12 +228,8 @@ function App() {
             </div>
             <div className="">
          
-              <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
-                allowAllLobbyPeersToJoinRoom()
-              </button>
-              <button >
-                Skip
-              </button>
+         
+            
             </div>
           </center>
         </HuddleClientProvider>
